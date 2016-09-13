64bf167951772ea3544fbd9493539636ce954afb
Michał Turek <mturek@wikia-inc.com>
2016-09-13 11:24:23 +0200

    up to 34th

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,17 +3,24 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private var times: Int = 0) {
+
+    operator fun invoke(): Invokable {
+        times++
+        return this
+    }
+
+    fun  getNumberOfInvocations() = times
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change class Invokable to count the number of invocations (round brackets).
         Uncomment the commented code - it should return 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -2,9 +2,11 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
-class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+class LazyPropertyUsingDelegates(initializer: () -> Int) {
+    val lazyValue: Int by lazy(initializer)
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
